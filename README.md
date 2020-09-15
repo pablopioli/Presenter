6c00e64045fa3bbab3ae10e99f12a4dda0cf0daf
Pablo Pioli <15127047+pablopioli@users.noreply.github.com>
2020-09-15 18:14:20 -0300

    First complete version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Presenter
 A simple tool for presentations written in Blazor
+
+Sometimes you don't need full presentation software to show a couple of slides.
+A couple of images in folder, a Blazor app and a browser tab can be equally effective.
+
+Put the images in the presentation folder and use:
+* CTRL + Shift + S to go to the next image/slide
+* CTRL + Shift + A to go to the previouse image/slide
---
+++ src/Client/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace Presenter.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHotKeys();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
